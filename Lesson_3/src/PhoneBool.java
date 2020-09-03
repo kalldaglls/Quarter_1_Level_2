70b267deed636e57ecf04dd5f1c6e21d6162801a
kalldaglls <kalldaglls@mail.ru>
2020-09-03 21:32:40 +0300

    Номер 1 решена!

---
+++ Lesson_3/src/PhoneBool.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneBool {
+    //private String surname;
+   // private String phoneNumber;//может инт или интеджер сделать?
+    private Map<String, Set<String>> book = new HashMap<>();
+}
