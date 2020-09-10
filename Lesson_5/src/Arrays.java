4141595fdd3b7d3ccbcc5e7e66ae1c49853a77b5
kalldaglls <kalldaglls@mail.ru>
2020-09-10 18:35:49 +0300

    Пока без синхронайзд. Но время выполнения странное для двупоточного метода - 21

--- Lesson_5/src/Arrays.java
+++ Lesson_5/src/Arrays.java
@@ -1,34 +1,68 @@
-public class Arrays {
+import static jdk.internal.misc.InnocuousThread.newThread;
+
+public class Arrays{
     static final int size = 10000000;
     static final int h = size / 2;
     float[] arr = new float[size];
     float [] a1 = new float[h];
     float [] a2 = new float[h];
 
+    public Arrays() {
+    }
 
-
-    public void playArray (float[] arr) {
+    public void playArray () {
         java.util.Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
-        //System.out.println(a);
+        System.out.println("Долгий метод " + a);
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println(System.currentTimeMillis() - a);
+        long b = System.currentTimeMillis();
+        System.out.println("В конце долгого метода " + b);
+        System.out.println(b - a);
     }
 
-    public void playShortArray (float[] arr, float [] aMain) {
+    public void playShortArray () {
         java.util.Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
         //System.out.println(a);
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
-        for (int i = 0; i < aMain.length; i++) {
-            aMain[i] = (float) (aMain[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("Поток № 1 короткого метода");
+                    for (int i = 0; i < a1.length; i++) {
+                        a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+                    }
+
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                    System.out.println("Поток № 2 короткого метода");
+                    for (int i = 0; i < a2.length; i++) {
+                        a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+                    }
+                    System.out.println(java.util.Arrays.toString(a2));
+                }
+            }).start();
+
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println(java.util.Arrays.toString(arr));
+        System.out.println("wait for it...");
+        long b = System.currentTimeMillis();
+        System.out.println("here is " + b);
+        System.out.println(b - a);
     }
 
     @Override
@@ -38,7 +72,9 @@ public class Arrays {
                 '}';
     }
 
-   // public void showInfo ()
+   public void showInfo () {
+       java.util.Arrays.toString(arr);
+   }
 
     public float[] getArr() {
         return arr;
@@ -51,4 +87,7 @@ public class Arrays {
     public float[] getA2() {
         return a2;
     }
+
+
+
 }
--- Lesson_5/src/Main.java
+++ Lesson_5/src/Main.java
@@ -1,25 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread());
-       Arrays arrays = new Arrays();
-       arrays.playArray(arrays.getArr());
+       new Arrays().playArray();
+       new Arrays().playShortArray();
        //System.out.println(arrays.toString());
-        new Thread(new TestThread()).start();
-        new Thread(new TestThread()).start();
     }
 
-    static class TestThread implements Runnable {
-
-        @Override
-        public void run() {
-            Arrays arrays1 = new Arrays();
-            Arrays arrays2 = new Arrays();
-            arrays1.playShortArray(arrays1.getArr(),arrays1.getA1());
-            arrays2.playShortArray(arrays2.getArr(),arrays2.getA2());
-
-        }
-    }
 
 
 }
