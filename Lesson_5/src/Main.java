d215227ea3de7a9346d185b5a8db705b66dfd3c7
kalldaglls <kalldaglls@mail.ru>
2020-09-13 21:58:25 +0300

    Сделал потоки и synchronized

--- Lesson_5/src/Main.java
+++ Lesson_5/src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-       new Arrays().playArray();
-       new Arrays().playShortArray();
+       //new Arrays().playArray();
+      // new Arrays().playShortArray();
+       new SyncArrays().playArraySync();
+       new SyncArrays().playShortArraySync();
        //System.out.println(arrays.toString());
     }
 
