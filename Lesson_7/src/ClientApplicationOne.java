c9187d50a177d9c9feb9e994e31b4a686146af6d
kalldaglls <kalldaglls@mail.ru>
2020-09-17 12:24:31 +0300

    Сделал как смог тайм-аут в классе ClientHandler в методе authenticate. Вроде худо-бедно работает. Так и не прудмал до конца, как корректно остановить один поток, если другой выполнился.

---
+++ Lesson_7/src/ClientApplicationOne.java
@@ -0,0 +1,40 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientApplicationOne {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 8888);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeUTF("");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String message = in.readUTF();
+                            System.out.println(message);
+                            if (message.contains("Incorrect credentials")) {
+                                out.writeUTF("-auth l1 p1");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+
+                    }
+                }
+            }).start();
+
+            out.writeUTF("-exit");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+    }
+
